304bd3c373c06b2043a51294d7993f94954be9d8
Sudharsan <sudharsan.0125@gmail.com>
2021-08-05 09:12:53 +0530

    ColouredText in python

---
+++ ColouredText.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLACK+Back.WHITE+"My name is Sudharsan")
+print(Back.BLACK+Fore.RED+"I Study Python")
